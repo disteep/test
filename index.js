e05177b9fc8831f3b92989c782e77ebf00978853
Ilya_N <in@juicyscore.com>
2021-06-01 12:24:46 +0300

    asd

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const test = () => {
     //commentary from local to 2000 MASTER CHANGE
     //asd
 }
+const res = fetch('https://score.juicyscore.com/static/js.js').then(data=>{console.log(data)})
+console.log(res)
