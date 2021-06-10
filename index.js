759a9d9c4835d472c0db515b28932a147c4cc05f
Ilya_N <in@juicyscore.com>
2021-06-10 16:05:19 +0300

    DEV-1337

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const test = () => {
     //commentary from local to 2000 MASTER CHANGE
     //asd
 }
-const res = fetch('https://score.juicyscore.com/static/js.js').then(data=>{console.log(data)})
+const res = fetch('https://score.juaaaaaaaasicyscore.com/static/js.js').then(data=>{console.log(data)})
 console.log(res)
